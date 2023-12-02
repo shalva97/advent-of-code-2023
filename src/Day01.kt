57ca28015e437e4ae7eb1ffcfce962e3956d14f3
shalva97 <shalva258@gmail.com>
2023-12-02 22:08:02 +0400

    Complete day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map {
+            it
+                .replace("eightwo", "82")
+                .replace("oneight", "18")
+                .replace("twone", "21")
+                .replace("threeight", "38")
+                .replace("fiveight", "58")
+                .replace("sevenine", "79")
+                .replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9")
+        }.run(::part1)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput).println()
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part2(testInput).println()
 }
